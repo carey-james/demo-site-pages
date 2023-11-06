188f06d6cd3c69fe7dbead2c6da3d217ad29f507
James Carey <57900176+carey-james@users.noreply.github.com>
2023-11-06 09:01:49 -0500

    Started adding more wizard format tests.
    WIP

--- src/wizard_tests/WizardFormatTests.jsx
+++ src/wizard_tests/WizardFormatTests.jsx
@@ -7,11 +7,20 @@ export default function WizardFormatTests(fileContents) {
 		errors.push({variant:'danger', title:'Missing Reserved Columns', text:'Please ensure the first eight (8) columns of your file match the following headers: Partner ID | Tax Year | Social Security Number | First Name | Middle Initial | Last Name | State | ZIP Code'});
 	}
 
-	// Test for more variable columns
+	// Test for more variable columns, to ensure there are by-groups
 	if ([...fileContents.matchAll('|')].length < 9) {
 		errors.push({variant:'warning', title:'Missing Group By Columns', text:'Your file does not have any "Group By" columns  after the Reserved Columns and will not return detailed match results from SQS.'});
 	}
 
+	//Line-by-line tests
+	const lines = fileContents.split('\n');
+
+	//Test for same number of columns
+	var col_errors = []
+	for (let )
+
+
+	// If no errors
 	if (errors.length === 0) {
 		errors.push({variant:'success', title:'No Errors!', text:'Horray! No errors were found!'});
 	}
